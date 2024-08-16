107c35623e35040bdcd385dd909e5380626a7b5d
gabrielsalazar225 <165944712+gabrielsalazar225@users.noreply.github.com>
2024-08-15 22:52:27 -0400

    Creating routes

--- models/Tag.js
+++ models/Tag.js
@@ -1,12 +1,21 @@
 const { Model, DataTypes } = require('sequelize');
-
 const sequelize = require('../config/connection.js');
-
 class Tag extends Model {}
 
 Tag.init(
   {
     // define columns
+id: {
+  type: DataTypes.INTEGER,
+  primaryKey: true,
+  autoIncrement: true,
+  allowNull: false
+},
+
+tag_name: {
+  type: DataTypes.INTEGER,
+  allowNull: true
+}
   },
   {
     sequelize,
